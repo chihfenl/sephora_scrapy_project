c34d278c730a5d7ceac11790a51b2cf7bc5ee266
Chih-Feng Lin <chihfenl@alumni.cmu.edu>
2019-05-16 19:20:45 -0400

    parse item name first

--- sephora_scrapy_project/spiders/sephora.py
+++ sephora_scrapy_project/spiders/sephora.py
@@ -1,21 +1,45 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
 class SephoraSpider(CrawlSpider):
     name = 'sephora'
-    allowed_domains = ['www.sephora.com/brands-list']
-    start_urls = ['http://www.sephora.com/brands-list/']
+    allowed_domains = ['sephora.com']
+    start_urls = ['http://www.sephora.com/brands-list']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow=".*",
+                restrict_xpaths="//li[@class='css-1hhsxaa ']/a",
+            ),
+            follow=True
+        ),
+        Rule(
+            LinkExtractor(
+                allow=".*",
+                restrict_xpaths="//div[@class='css-1hnm2t1 ']" +
+                                "//a[contains(@href, 'all')]",
+            ),
+            follow=True
+        ),
+        Rule(
+            LinkExtractor(
+                allow=".*",
+                restrict_xpaths="//div[@class='css-12egk0t']" +
+                                "//a[@class='css-ix8km1']",
+            ),
+            follow=True,
+            callback='parse_item'
+        )
+    ]
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+
+        item_name = response.xpath(
+            "//span[@class='css-0']//text()"
+        ).extract_first()
+        yield {
+            "item_name": item_name
+        }
